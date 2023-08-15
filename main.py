35f14dd623c55a82067ac809727b7a44f038c43f
Gonçalo Baptista <goncalo.baptista@sky.uk>
2023-08-15 13:59:53 +0100

    Remove output format and existing info; add info (ratios too) in new format

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+
 import pandas as pd
 import undetected_chromedriver as uc
 from selenium import webdriver
@@ -7,9 +8,9 @@ from selenium.webdriver.remote.webelement import WebElement
 
 # Constants for XPath expressions
 FINANCIALS_XPATHS = {
-    "income": "/html/body/div[1]/div[2]/div[2]/div[2]/div/div",
+    "income_statement": "/html/body/div[1]/div[2]/div[2]/div[2]/div/div",
     "balance_sheet": "/html/body/div[1]/div[2]/div[2]/div[3]/div/div",
-    "cash_flow": "/html/body/div[1]/div[2]/div[2]/div[4]/div/div",
+    "cash_flow_statement": "/html/body/div[1]/div[2]/div[2]/div[4]/div/div",
 }
 
 RATIOS_XPATHS = {
@@ -39,7 +40,9 @@ def get_years_from_table_header(table_header: WebElement):
 def parse_table(driver, table_xpath):
     table = driver.find_element(By.XPATH, table_xpath)
     (table_header, table_rows_frame) = get_children_elements(table)
+
     years = get_years_from_table_header(table_header)
+    metrics_names = []
     data = []
 
     rows_elements = get_children_elements(table_rows_frame)
@@ -48,38 +51,83 @@ def parse_table(driver, table_xpath):
         metric_name = columns[0].text
         metrics_values = []
 
+        metrics_names.append(metric_name)
+
         for column in columns[1:]:
             metric_value = column.text.replace(",", "")
-            if metric_value == "- -" or metric_value == "12345" or metric_value == "":
+
+            if metric_value == "- -":
                 metric_value = "0"
+
+            if metric_value == "12345" or metric_value == "":
+                continue
+
             metrics_values.append(metric_value)
 
         data.append(metrics_values)
 
     data = [data[i][-10:] for i in range(len(data))]
-    dataframe = pd.DataFrame(data=data, columns=years,
-                             index=[metric_name for metric_name in get_children_elements(table_rows_frame)])
+    dataframe = pd.DataFrame(data=data, columns=years, index=metrics_names)
     return dataframe
 
 
-def scrape_data(driver, tickers, period, xpath_dict, parent_dir):
+def is_us_company(driver):
+    xpath = "/html/body/div[1]/div[2]/div[2]/div/div"
+    element = driver.find_elements(By.XPATH, xpath)
+
+    if len(element) == 1:
+        return False
+    return True
+
+
+def get_remaining_financial_types(ticker, financial_types):
+    remaining_financial_types = []
+    for financial_type in financial_types:
+        directory = f"output/{ticker}/{financial_type}"
+        if not os.path.exists(directory):
+            remaining_financial_types.append(financial_type)
+    return remaining_financial_types
+
+
+def scrape_data(driver, tickers, period):
+    financial_types = ["financials", "ratios"]
+
     for ticker in tickers:
-        print(f"Scraping {parent_dir} of {ticker}...")
-        directory = f"output/{parent_dir}/{ticker}"
-        if os.path.exists(directory):
-            print(f"Directory {directory} already exists, skipping...")
+        skip_non_us_companies = False
+        remaining_financial_types = get_remaining_financial_types(ticker, financial_types)
+
+        if len(remaining_financial_types) == 0:
+            print(f"Skipping {ticker}...")
             continue
 
-        url = f"https://roic.ai/{parent_dir}/{ticker}?fs={period}"
-        driver.get(url)
+        for financial_type in remaining_financial_types:
 
-        try:
-            for element_type, element_xpath in xpath_dict.items():
-                statement_df = parse_table(driver, element_xpath)
-                os.makedirs(directory, exist_ok=True)
-                statement_df.to_csv(f"{directory}/{element_type}_{parent_dir}.csv")
-        except Exception as e:
-            print(f"Error scraping {ticker}: {e}")
+            if skip_non_us_companies:
+                break
+
+            if financial_type == "financials":
+                xpath_dict = FINANCIALS_XPATHS
+            else:
+                xpath_dict = RATIOS_XPATHS
+
+            print(f"Scraping {financial_type} of {ticker}...")
+
+            url = f"https://roic.ai/{financial_type}/{ticker}?fs={period}"
+            driver.get(url)
+
+            if not is_us_company(driver):
+                print(f"[WARNING] Company {ticker} is not a US company, skipping...")
+                skip_non_us_companies = True
+                continue
+
+            directory = f"output/{ticker}/{financial_type}"
+            try:
+                for element_type, element_xpath in xpath_dict.items():
+                    statement_df = parse_table(driver, element_xpath)
+                    os.makedirs(directory, exist_ok=True)
+                    statement_df.to_csv(f"{directory}/{element_type}.csv")
+            except Exception as e:
+                print(f"Error scraping {ticker}: {e}")
 
 
 def get_sp500_tickers():
@@ -89,11 +137,15 @@ def get_sp500_tickers():
     return sp500_df["Symbol"].tolist()
 
 
-if __name__ == "__main__":
+def create_chrome_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("start-maximized")
-    driver = uc.Chrome(options=options)
+    return uc.Chrome(options=options)
 
+
+if __name__ == "__main__":
+    driver = create_chrome_driver()
     symbols = get_sp500_tickers()
-    scrape_data(driver, symbols, "annual", FINANCIALS_XPATHS, "financials")
-    scrape_data(driver, symbols, "annual", RATIOS_XPATHS, "ratios")
+    period = "annual"
+
+    scrape_data(driver, symbols, period)
