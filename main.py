e43e336bf17387b6751e3f7274a330c9d1a213de
Gonçalo Baptista <goncalo.baptista@sky.uk>
2023-08-15 02:03:27 +0100

    Add Ratios scraping capability and improve code.

--- main.py
+++ main.py
@@ -1,111 +1,85 @@
 import os
-
 import pandas as pd
+import undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
-import undetected_chromedriver as uc
 
 # Constants for XPath expressions
-INCOME_STATEMENT_XPATH = "/html/body/div[1]/div[2]/div[2]/div[2]/div/div"
-BALANCE_SHEET_XPATH = "/html/body/div[1]/div[2]/div[2]/div[3]/div/div"
-CASH_FLOW_XPATH = "/html/body/div[1]/div[2]/div[2]/div[4]/div/div"
-STATEMENT_XPATHS = {
-    "income": INCOME_STATEMENT_XPATH,
-    "balance_sheet": BALANCE_SHEET_XPATH,
-    "cash_flow": CASH_FLOW_XPATH,
+FINANCIALS_XPATHS = {
+    "income": "/html/body/div[1]/div[2]/div[2]/div[2]/div/div",
+    "balance_sheet": "/html/body/div[1]/div[2]/div[2]/div[3]/div/div",
+    "cash_flow": "/html/body/div[1]/div[2]/div[2]/div[4]/div/div",
+}
+
+RATIOS_XPATHS = {
+    "profitability": "/html/body/div[1]/div[2]/div[2]/div[2]/div/div",
+    "credit": "/html/body/div[1]/div[2]/div[2]/div[3]/div/div",
+    "liquidity": "/html/body/div[1]/div[2]/div[2]/div[4]/div/div",
+    "working_capital": "/html/body/div[1]/div[2]/div[2]/div[5]/div/div",
+    "enterprise_value": "/html/body/div[1]/div[2]/div[2]/div[6]/div/div",
+    "multiples": "/html/body/div[1]/div[2]/div[2]/div[7]/div/div",
+    "per_share": "/html/body/div[1]/div[2]/div[2]/div[8]/div/div",
 }
 
 
 def get_children_elements(element: WebElement):
-    return element.find_elements(By.XPATH, f"./*")
+    return element.find_elements(By.XPATH, "./child::*")
 
 
 def get_years_from_table_header(table_header: WebElement):
     years = []
     for column in get_children_elements(table_header):
-        if column == get_children_elements(table_header)[0]:
-            continue
-
         year = column.text.replace(" Y", "")
-
-        if year == "TTM" or not year.isdigit():
-            continue
-
-        years.append(year)
-    return years
+        if year.isdigit():
+            years.append(year)
+    return years[-10:]
 
 
-def parse_statement(driver, statement_xpath):
-    years = []
-    metrics_names = []
+def parse_table(driver, table_xpath):
+    table = driver.find_element(By.XPATH, table_xpath)
+    (table_header, table_rows_frame) = get_children_elements(table)
+    years = get_years_from_table_header(table_header)
     data = []
 
-    statement_table = driver.find_element(By.XPATH, statement_xpath)
-    (table_header, table_rows_frame) = get_children_elements(statement_table)
-
-    years = get_years_from_table_header(table_header)
-
     rows_elements = get_children_elements(table_rows_frame)
-
     for row in rows_elements:
-        metrics_values = []
         columns = get_children_elements(row)
         metric_name = columns[0].text
-
-        metrics_names.append(metric_name)
+        metrics_values = []
 
         for column in columns[1:]:
             metric_value = column.text.replace(",", "")
-
-            if metric_value == "- -":
+            if metric_value == "- -" or metric_value == "12345" or metric_value == "":
                 metric_value = "0"
-
-            if metric_value == '12345' or metric_value == '':
-                continue
-
             metrics_values.append(metric_value)
 
         data.append(metrics_values)
 
-    # Trim all data lists to only containing the last 10 years
     data = [data[i][-10:] for i in range(len(data))]
-
-    # Same for years
-    years = years[-10:]
-
-    dataframe = pd.DataFrame(data=data, columns=years, index=metrics_names)
+    dataframe = pd.DataFrame(data=data, columns=years,
+                             index=[metric_name for metric_name in get_children_elements(table_rows_frame)])
     return dataframe
 
 
-def scrape_financial_data(tickers, period):
-    options = webdriver.ChromeOptions()
-    options.add_argument("start-maximized")
-    driver = uc.Chrome(options=options)
-
+def scrape_data(driver, tickers, period, xpath_dict, parent_dir):
     for ticker in tickers:
-        print(f"Scraping {ticker} financial data...")
-        directory = f"output/{ticker}"
-        # If directory already exists, skip; else, create it
+        print(f"Scraping {parent_dir} of {ticker}...")
+        directory = f"output/{parent_dir}/{ticker}"
         if os.path.exists(directory):
             print(f"Directory {directory} already exists, skipping...")
             continue
 
-        # driver = webdriver.Chrome()
-
-        url = f"https://roic.ai/financials/{ticker}?fs={period}"
+        url = f"https://roic.ai/{parent_dir}/{ticker}?fs={period}"
         driver.get(url)
 
         try:
-            for statement_type, statement_xpath in STATEMENT_XPATHS.items():
-                statement_df = parse_statement(driver, statement_xpath)
+            for element_type, element_xpath in xpath_dict.items():
+                statement_df = parse_table(driver, element_xpath)
                 os.makedirs(directory, exist_ok=True)
-                statement_df.to_csv(f"{directory}/{statement_type}_statement.csv")
+                statement_df.to_csv(f"{directory}/{element_type}_{parent_dir}.csv")
         except Exception as e:
             print(f"Error scraping {ticker}: {e}")
-        finally:
-            # driver.close()
-            pass
 
 
 def get_sp500_tickers():
@@ -116,5 +90,10 @@ def get_sp500_tickers():
 
 
 if __name__ == "__main__":
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    driver = uc.Chrome(options=options)
+
     symbols = get_sp500_tickers()
-    scrape_financial_data(symbols, "annual")
+    scrape_data(driver, symbols, "annual", FINANCIALS_XPATHS, "financials")
+    scrape_data(driver, symbols, "annual", RATIOS_XPATHS, "ratios")
